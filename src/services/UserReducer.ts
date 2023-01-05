fe88817cbbd8149ec5888062c17f6ca11a257221
imkim1893 <imkim1893@naver.com>
2023-01-05 16:15:09 +0900

    redux 적용

--- src/services/UserReducer.ts
+++ src/services/UserReducer.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { ReduxUtil } from "_commons/utils/ReduxUtil";
 
-const _uniqueKey = "userReducer";
+const _reducerKey = "userReducer";
 
 const initialState = {
   version: 3,
@@ -9,7 +9,7 @@ const initialState = {
 
 const actions = {
   login: createAsyncThunk(
-    `${_uniqueKey}/login`,
+    `${_reducerKey}/login`,
     async (params) => {
       //TODO: params를 통해 API 작업 구현
 
@@ -23,13 +23,13 @@ const actions = {
 //------------------------------ 위에는 수정 O, 아래는 수정 X
 type STATE = typeof initialState;
 const _slice = createSlice({
-  name: _uniqueKey,
+  name: _reducerKey,
   initialState: initialState,
   reducers: {},
   extraReducers: ReduxUtil.makeExtraReducers(actions),
 });
 export default class UserReducer {
-  static readonly key: string = _uniqueKey;
+  static readonly key: string = _reducerKey;
   static readonly reducer = _slice.reducer;
   static readonly actions = actions;
 }
