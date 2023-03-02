0e2f3a021785b23ff3d4c1a48078cb8f0afe5c35
TryIt <imkim1893@naver.com>
2023-03-03 00:19:56 +0900

    부족하지만 타이핑 완성

--- src/screens/widgets/portpolio2/HomePage.tsx
+++ src/screens/widgets/portpolio2/HomePage.tsx
@@ -1,8 +1,32 @@
-import { css } from "@emotion/css";
+import { css, keyframes } from "@emotion/css";
 import React from "react";
 import MyColors from "_commons/MyColors";
 import { ElevatedButton } from "_commons/widgets/ElevatedButton";
 import { SizedBox } from "_commons/widgets/SizedBox";
+
+const typingEffectByWidth = keyframes`
+  0% {
+    width: 100%;
+  }
+  50% {
+    width: 45%;
+  }
+  100% {
+    width: 100%;
+  }
+`;
+
+const typingFlicker = keyframes`
+  0% {
+    border-right: 1.5px solid black;
+  }
+  50% {
+    border-right: 0px;
+  }
+  100% {
+    border-right: 1.5px solid black;
+  }
+`;
 
 //https://preview.themeforest.net/item/alexio-tailwind-css-personal-portfolio/full_screen_preview/42428238?_ga=2.164459583.185243531.1672614639-1925763281.1672614639&_gac=1.158435912.1672614639.CjwKCAiA-8SdBhBGEiwAWdgtcCYxLI1eiPFynnSVXXt_SSTLIgX795ismy3VyyukBFk-VIbk__HMzBoCfsIQAvD_BwE
 const HomePage = React.memo(() => {
@@ -49,16 +73,37 @@ const HomePage = React.memo(() => {
       >
         <div
           className={css`
+            position: relative;
             font-size: 18px;
+            ::after {
+              content: ""; //after는 content가 있어야 동작한다.
+              position: absolute;
+              top: -10px;
+              left: -10px;
+              height: 40px;
+              width: 40px;
+              border-radius: 50%;
+              background-color: rgb(238 148 147 / 0.3);
+            }
           `}
         >
-          {" "}
           HELLO, THERE
         </div>
         <SizedBox height="30px" />
         <div
           className={css`
+            position: relative;
             font-size: 60px;
+            ::after {
+              content: "";
+              position: absolute;
+              top: 20px;
+              right: -10px;
+              height: 40px;
+              width: 40px;
+              border-radius: 50%;
+              background-color: rgb(96 165 250 / 0.5);
+            }
           `}
         >
           I'M ALEXIS LARTEN
@@ -66,13 +111,50 @@ const HomePage = React.memo(() => {
         <SizedBox height="30px" />
         <div
           className={css`
-            font-size: 18px;
+            position: relative;
+            font-size: 20px;
+            white-space: nowrap;
+            color: transparent;
+            ::before {
+              content: "APP/WEB DEVELOPER";
+              color: black;
+
+              //현재 div에 대해 100%의 크기를 갖도록 하는것이 목적
+              position: absolute;
+              width: 100%;
+              height: 100%;
+              left:0px;
+
+              overflow: hidden;
+              //5초동안 31단계를 통해 typing을 보여줘. 계속. (31단계가, 뚝뚝 끊기는 느낌을 줌.)
+              animation: ${typingEffectByWidth} 3s steps(10) infinite;
+            }
+            ::after {
+              content: "";
+              color: black;
+
+              //현재 div에 대해 100%의 크기를 갖도록 하는것이 목적
+              position: absolute;
+              width: 100%;
+              height: 100%;
+              left:0px;
+              margin-left: 15px;
+
+              overflow: hidden;
+              border-right: 1.5px solid black;
+              //5초동안 31단계를 통해 typing을 보여줘. 계속. (31단계가, 뚝뚝 끊기는 느낌을 줌.)
+              animation: ${typingEffectByWidth} 3s steps(10) infinite,  ${typingFlicker} 2s infinite;
+            }
           `}
         >
-          App/Web Developer
+          APP/WEB DEVELOPER
         </div>
         <SizedBox height="30px" />
-        <ElevatedButton backgroundColor={MyColors.pink} hoverBackgroundColor={MyColors.black} color={MyColors.white}>
+        <ElevatedButton
+          backgroundColor={MyColors.pink}
+          hoverBackgroundColor={MyColors.black}
+          color={MyColors.white}
+        >
           Download CV
         </ElevatedButton>
       </div>
