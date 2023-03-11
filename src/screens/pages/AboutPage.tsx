16d71346c0db368eb207f78253b637cf0b75fee9
TryIt <imkim1893@naver.com>
2023-03-11 23:47:56 +0900

    대충 resumePage 추가

--- src/screens/pages/AboutPage.tsx
+++ src/screens/pages/AboutPage.tsx
@@ -13,6 +13,7 @@ import {
 } from "react-icons/bs";
 import BigSubjectAboveImage from "screens/widgets/BigSubjectAboveImage";
 import LAndLongR from "screens/widgets/LAndLongR";
+import HeaderCard from "screens/widgets/HeaderCard";
 
 const AboutPage = React.memo(() => {
   return (
@@ -32,7 +33,7 @@ const AboutPage = React.memo(() => {
       }
       right={
         <>
-          <Title></Title>
+          <HeaderCard title={"ABOUT"} subtitle={"WHO I AM"}></HeaderCard>
           <IntroductionCard></IntroductionCard>
           <NumberCardWrap></NumberCardWrap>
         </>
@@ -41,96 +42,6 @@ const AboutPage = React.memo(() => {
   );
 });
 
-const Title = () => {
-  return (
-    <>
-      <h6
-        className={[
-          css`
-            position: relative;
-            z-index: 1;
-            margin-bottom: 8px;
-            display: inline-block;
-            padding-left: 10px;
-            padding-right: 10px;
-            padding-top: 3px;
-            padding-bottom: 3px;
-            font-size: 11px;
-            text-transform: uppercase;
-            letter-spacing: 3px;
-            --tw-text-opacity: 1;
-            color: rgb(0 0 0 / var(--tw-text-opacity));
-
-            ::after {
-              content: "";
-              position: absolute;
-              top: 0px;
-              bottom: 0px;
-              left: 0px;
-              right: 0px;
-              z-index: -1;
-              --tw-rotate: 0;
-              --tw-skew-x: -20deg;
-              --tw-skew-y: 0;
-              --tw-scale-x: 1;
-              --tw-scale-y: 1;
-              --tw-translate-x: 0;
-              --tw-translate-y: 0;
-              transform: translate(var(--tw-translate-x), var(--tw-translate-y))
-                rotate(var(--tw-rotate)) skewX(var(--tw-skew-x))
-                skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x))
-                scaleY(var(--tw-scale-y));
-              --tw-bg-opacity: 1;
-              background-color: rgb(238 148 147 / var(--tw-bg-opacity));
-            }
-          `,
-          "font-bold",
-        ].join(" ")}
-      >
-        WHO I AM
-      </h6>
-      <h2
-        className={css`
-          position: relative;
-          z-index: 1;
-          margin: 0px;
-          display: inline-block;
-          border-bottom-width: 1px;
-          --tw-border-opacity: 1;
-          border-color: rgb(229 231 235 / var(--tw-border-opacity));
-          padding-bottom: 10px;
-          font-size: 45px;
-          font-weight: 700;
-          text-transform: uppercase;
-          line-height: 1.1;
-          --tw-text-opacity: 1;
-          color: rgb(0 0 0 / var(--tw-text-opacity));
-          margin-bottom: 40px;
-
-          ::after {
-            content: var(--tw-content);
-            position: absolute;
-            bottom: -4px;
-            left: 0px;
-            right: 0px;
-            z-index: -1;
-            height: 8px;
-            width: 50px;
-            --tw-skew-x: -36deg;
-            transform: translate(var(--tw-translate-x), var(--tw-translate-y))
-              rotate(var(--tw-rotate)) skewX(var(--tw-skew-x))
-              skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x))
-              scaleY(var(--tw-scale-y));
-            --tw-bg-opacity: 1;
-            background-color: rgb(238 148 147 / var(--tw-bg-opacity));
-          }
-        `}
-      >
-        ABOUT
-      </h2>
-    </>
-  );
-};
 
 class Skill {
   name: string;
--- src/screens/pages/ResumePage.tsx
+++ src/screens/pages/ResumePage.tsx
@@ -1,6 +1,7 @@
 import { css } from "@emotion/css";
 import React from "react";
 import BigSubjectAboveImage from "screens/widgets/BigSubjectAboveImage";
+import HeaderCard from "screens/widgets/HeaderCard";
 import LAndLongR from "screens/widgets/LAndLongR";
 
 const ResumePage = React.memo(() => {
@@ -21,12 +22,165 @@ const ResumePage = React.memo(() => {
       }
       right={
         <>
-          {/* <Title></Title>
-          <IntroductionCard></IntroductionCard>
-          <NumberCardWrap></NumberCardWrap> */}
+          <HeaderCard title={"MY RESUME"} subtitle={"HISTORY"}></HeaderCard>
+          <MyExperience></MyExperience>
+          <MySkills></MySkills>
         </>
       }
     ></LAndLongR>
+  );
+});
+
+const MyExperience = React.memo(() => {
+  return (
+    <div className="grid grid-cols-12 gap-7">
+      <div className="col-span-12 lg:col-span-6">
+        <div className="relative">
+          <h2>Experience</h2>
+          <ul>
+            <li>
+              <div className="r-name">
+                <i className="ti-briefcase"></i>
+                <span>Google</span>
+                <label>OCT 2015 - JUNE 2016</label>
+              </div>
+              <div className="r-info">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo
+                </p>
+              </div>
+            </li>
+            <li>
+              <div className="r-name">
+                <i className="ti-briefcase"></i>
+                <span>Apple</span>
+                <label>OCT 2015 - JUNE 2016</label>
+              </div>
+              <div className="r-info">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="col-span-12 lg:col-span-6">
+        <div className="resume-row">
+          <h2>Education</h2>
+          <ul>
+            <li>
+              <div className="r-name">
+                <i className="fas fa-graduation-cap"></i>
+                <span>University</span>
+                <label>OCT 2015 - JUNE 2016</label>
+              </div>
+              <div className="r-info">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo
+                </p>
+              </div>
+            </li>
+            <li>
+              <div className="r-name">
+                <i className="fas fa-graduation-cap"></i>
+                <span>Design and Art</span>
+                <label>OCT 2015 - JUNE 2016</label>
+              </div>
+              <div className="r-info">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+});
+
+class SkillSlider {
+  name: string;
+  percent: number;
+
+  constructor(name: string, percent: number) {
+    this.name = name;
+    this.percent = percent;
+  }
+}
+
+const MySkills = React.memo(() => {
+  const skillsByGroup: Record<string, SkillSlider[]> = {
+    "Design Skills": [
+      new SkillSlider("CORAL DRAW", 92),
+      new SkillSlider("CORAL DRAW", 92),
+      new SkillSlider("CORAL DRAW", 92),
+    ],
+    "Coding Skills": [
+      new SkillSlider("CORAL DRAW", 92),
+      new SkillSlider("CORAL DRAW", 92),
+      new SkillSlider("CORAL DRAW", 92),
+    ],
+  };
+
+  return (
+    <div className="w-full mt-[30px]">
+      <div className="sub-title mb-[30px]">
+        <h2>My Skills</h2>
+      </div>
+      <div className="grid grid-cols-12 gap-7">
+        {Object.entries(skillsByGroup).map((entry) => {
+          return (
+            <div className="col-span-12 lg:col-span-6">
+              <h3>{entry[0]}</h3>
+              <div className="skills">
+                {entry[1].map((e) => {
+                  return (
+                    <>
+                      <div className="progress-lt">
+                        <h6>{e.name}</h6>
+                        <span>{e.percent}%</span>
+                        <div className="progress">
+                          <div
+                            className={css`
+                              --tw-bg-opacity: 1;
+                              background-color: rgb(
+                                0 0 0 / var(--tw-bg-opacity)
+                              );
+                              transition-duration: 300ms;
+                              transition-timing-function: cubic-bezier(
+                                0.4,
+                                0,
+                                0.2,
+                                1
+                              );
+                              width: ${e.percent}%;
+                            `}
+                          ></div>
+                        </div>
+                      </div>
+                    </>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 });
 
