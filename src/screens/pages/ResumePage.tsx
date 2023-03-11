491ab1083eb6e747fb8d646337d2c82360f8fa1a
TryIt <imkim1893@naver.com>
2023-03-11 23:11:01 +0900

    ResumePage 대략 완성

--- src/screens/pages/ResumePage.tsx
+++ src/screens/pages/ResumePage.tsx
@@ -1,18 +1,33 @@
 import { css } from "@emotion/css";
 import React from "react";
+import BigSubjectAboveImage from "screens/widgets/BigSubjectAboveImage";
+import LAndLongR from "screens/widgets/LAndLongR";
 
-const Page1 = React.memo(() => {
+const ResumePage = React.memo(() => {
   return (
-    <div
-      className={css`
-        width: 100%;
-        height: 100%;
-        background-color: pink;
-      `}
-    >
-      ResumePage
-    </div>
+    <LAndLongR
+      left={
+        <>
+          <img
+            className={css`
+              height: 100%;
+              object-fit: cover;
+            `}
+            alt="profile"
+            src="https://images.unsplash.com/photo-1455390582262-044cdead277a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
+          />
+          <BigSubjectAboveImage>MY RESUME</BigSubjectAboveImage>
+        </>
+      }
+      right={
+        <>
+          {/* <Title></Title>
+          <IntroductionCard></IntroductionCard>
+          <NumberCardWrap></NumberCardWrap> */}
+        </>
+      }
+    ></LAndLongR>
   );
 });
 
-export default Page1;
+export default ResumePage;
