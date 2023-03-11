01b0e87172340461f1949115bd78e42584f0a4d4
TryIt <imkim1893@naver.com>
2023-03-11 22:14:42 +0900

    메뉴 기능 추가 완료

--- src/screens/layouts/MainLayout.tsx
+++ src/screens/layouts/MainLayout.tsx
@@ -34,7 +34,7 @@ const Menu = React.memo(() => {
   return (
     <nav
       className={css`
-        ${isMenuOn?'z-index: 1;':''}
+        ${isMenuOn ? "z-index: 1;" : ""}
         position: absolute;
         top: 0;
         width: 100vw;
@@ -55,29 +55,60 @@ const Menu = React.memo(() => {
       onClick={() => {}}
     >
       {menuList.map((menu, i) => (
-        <div
-          className={css`
-            ${isMenuOn ? 'cursor: pointer;': ''}//not working.. 이 때는, 해당 페이지가 뒤에 있기 때문일 수 있다.
+        <li
+          className={[
+            "relative",
+            css`
+              margin-left: 14px;
+              margin-right: 14px;
+            `,
+          ].join(" ")}
+        >
+          <div
+            className={css`
+              ${isMenuOn
+                ? "cursor: pointer;"
+                : ""} //not working.. 이 때는, 해당 페이지가 뒤에 있기 때문일 수 있다.
 
             color: white;
-            text-align: center;
-            font-size: 18px;
-            font-weight: 500;
-            text-transform: uppercase;
+              text-align: center;
+              font-size: 18px;
+              font-weight: 500;
+              text-transform: uppercase;
 
-            margin-left: 14px;
-            margin-right: 14px;
-          `}
-          onClick={() => {
-            dispatch(
-              PageReducer.actions.selectedScreenIndex({
-                selectedScreenIndex: i,
-              })
-            );
-          }}
-        >
-          {menu}
-        </div>
+              //왼쪽에서 오른쪽으로 흐르는 효과
+              ::after {
+                content: var(--tw-content);
+                position: absolute;
+                bottom: 0px;
+                left: auto;
+                right: 0px;
+                height: 1px;
+                width: 0px;
+
+                --tw-bg-opacity: 1;
+                background-color: rgb(255 255 255 / var(--tw-bg-opacity));
+                transition-duration: 300ms;
+                transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+              }
+              :hover::after {
+                content: var(--tw-content);
+                right: auto;
+                left: 0px;
+                width: 100%;
+              }
+            `}
+            onClick={() => {
+              dispatch(
+                PageReducer.actions.selectedScreenIndex({
+                  selectedScreenIndex: i,
+                })
+              );
+            }}
+          >
+            {menu}
+          </div>
+        </li>
       ))}
     </nav>
   );
@@ -164,36 +195,44 @@ const StackedScreen = React.memo(() => {
         perspective-origin: 50% -50%; //보고 있는 눈에 위치를 변경.
       `}
     >
-      {screenList.map(([selectedScreenIndex, screen]:[selectedScreenIndex:string,screen:any], i) => {
-        return (
-          <div
-            className={css`
-              ${isMenuOn ? 'cursor: pointer;': ''}
-              z-index: ${screenList.length - 1 - i};
-              position: absolute;
-              width: 100%;
-              height: 100%;
-              overflow: hidden;
+      {screenList.map(
+        (
+          [selectedScreenIndex, screen]: [
+            selectedScreenIndex: string,
+            screen: any
+          ],
+          i
+        ) => {
+          return (
+            <div
+              className={css`
+                ${isMenuOn ? "cursor: pointer;" : ""}
+                z-index: ${screenList.length - 1 - i};
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                overflow: hidden;
 
-              transition: transform 0.45s, opacity 0.45s;
-              opacity: ${1 - 0.1 * i};
-              transform: ${isMenuOn
-                ? `translate3d(0,75%,${-200 - i * 50}px)`
-                : undefined};
-            `}
-            onClick={() => {
-              dispatch(
-                PageReducer.actions.selectedScreenIndex({
-                  selectedScreenIndex: Number(selectedScreenIndex),
-                })
-              );
-            }}
-          >
-            {screen}
-            {i}
-          </div>
-        );
-      })}
+                transition: transform 0.45s, opacity 0.45s;
+                opacity: ${1 - 0.1 * i};
+                transform: ${isMenuOn
+                  ? `translate3d(0,75%,${-200 - i * 50}px)`
+                  : undefined};
+              `}
+              onClick={() => {
+                dispatch(
+                  PageReducer.actions.selectedScreenIndex({
+                    selectedScreenIndex: Number(selectedScreenIndex),
+                  })
+                );
+              }}
+            >
+              {screen}
+              {i}
+            </div>
+          );
+        }
+      )}
     </div>
   );
 });
